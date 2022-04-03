4ee47aae16976a6839019449483fae2061256474
knightfury1493 <suhaibahmmad1493@gmail.com>
2022-04-04 03:03:03 +0600

    Added sql host as env variable

--- server/sqlConnection.js
+++ server/sqlConnection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.SQL_Host,
     port: 3306,
     user: process.env.SQL_Username,
     password: process.env.SQL_Password,
