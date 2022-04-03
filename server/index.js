2d44c41cd427ca32ea2caa4b9bb6e554b012e564
knightfury1493 <suhaibahmmad1493@gmail.com>
2022-04-04 03:12:58 +0600

    Fixed port bugs!

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const grpc = require('grpc');
 const signIn = require('./rpc methods/signIn');
 const signUp = require('./rpc methods/signUp');
 const logout = require('./rpc methods/logout');
+require('dotenv').config();
 
 
 
 const server = new grpc.Server();
 server.addService(UserService.UserServiceService, {signUp:signUp, signIn:signIn,logout:logout});
-server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+server.bind(process.env.PORT || "127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
 server.start();
 
-console.log("Server is up and running in port 127.0.0.1:50051");
+console.log(`Server is up and running in port ${process.env.PORT}`);
